d4ef82804b00279c15d1a9dcef824e1e15cd44c4
Ivo <frostwolfe91@gmail.com>
2018-09-14 21:33:36 +0300

    Create default page vue element for website

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,6 +2,12 @@ import VueRouter from 'vue-router'
 
 const router = new VueRouter({
     mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: () => import('./layouts/default-page' /* webpackChunkName: "js/default-page" */)
+        }
+    ]
 });
 
 export default router;
