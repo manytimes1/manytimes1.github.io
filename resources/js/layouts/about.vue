4119b6ba36bbca89efe6796cc424103548f0d84b
Ivo <frostwolfe91@gmail.com>
2018-09-15 12:35:46 +0300

    Create "About me" section

---
+++ resources/js/layouts/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+            <h1 class="mb-0">Ivaylo
+                <span class="text-primary">Ivanov</span>
+            </h1>
+            <div class="subheading mb-5">Burgas, Bulgaria ·
+                <a href="mailto:name@email.com">frostwolfe91@gmail.com</a>
+            </div>
+            <p class="lead mb-5">I am studying Software Engineering in Software University.
+                Currently I am working like Web Development Freelancer.
+            </p>
+
+            <div class="social-icons">
+                <a href="https://www.linkedin.com/in/ivayloivv">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a href="https://github.com/manytimes1">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="https://www.facebook.com/n3cr0m4ncer">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
--- resources/js/layouts/default-page.vue
+++ resources/js/layouts/default-page.vue
@@ -1,13 +1,20 @@
 <template>
-    <nav-bar></nav-bar>
+    <div>
+        <nav-bar></nav-bar>
+
+        <div class="container-fluid p-0">
+            <about-me></about-me>
+        </div>
+    </div>
 </template>
 
 <script>
     import NavBar from './navbar'
+    import AboutMe from './about'
 
     export default {
         components: {
-            NavBar
+            NavBar, AboutMe
         }
     }
 </script>
