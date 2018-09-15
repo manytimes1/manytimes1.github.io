598dba73f9009db79e25e021cd30e71e8bd6ae03
Ivo <frostwolfe91@gmail.com>
2018-09-15 13:11:17 +0300

    * Create Spinning Preloader for website

---
+++ public/js/plugin.js
@@ -0,0 +1,1 @@
+$(function(){$(".preloader").fadeOut()});
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,9 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public');
 
 mix.js('resources/js/app.js', 'public/js')
+    .combine([
+        'resources/js/custom.js'
+    ],'public/js/plugin.js')
     .sass('resources/sass/style.scss', 'public/css')
     .browserSync('portfolio');
 
