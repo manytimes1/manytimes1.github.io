4ef01019343356525f1325bd80d48b1ed02868dc
Ivo <frostwolfe91@gmail.com>
2018-11-29 15:05:19 +0200

    Remove extra div tag

--- resources/js/layouts/default-page.vue
+++ resources/js/layouts/default-page.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-        <nav-bar></nav-bar>
+    <nav-bar></nav-bar>
 
-        <div class="container-fluid p-0">
-            <about-me></about-me>
-            <experience></experience>
-            <education></education>
-            <skills></skills>
-            <certifications></certifications>
-            <projects></projects>
-        </div>
+    <div class="container-fluid p-0">
+        <about-me></about-me>
+        <experience></experience>
+        <education></education>
+        <skills></skills>
+        <certifications></certifications>
+        <projects></projects>
     </div>
 </template>
 
