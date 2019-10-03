407e9a4183d748786f40dec00d0ad4f9133205f3
Ivo <frostwolfe91@gmail.com>
2019-10-03 15:32:59 +0300

    Update Certificates information

--- resources/js/layouts/certificates.vue
+++ resources/js/layouts/certificates.vue
@@ -1,16 +1,18 @@
 <template>
     <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="certificates">
-        <div class="my-auto">
-            <h2 class="mb-5">Certifications</h2>
-            <ul class="fa-ul mb-0">
-                <li>
-                    <i class="fa-li fa fa-trophy text-warning">
-                        <a href="https://softuni.bg/certificates/details/59153/6b5ef0a8">
-                            Programming Basics with C++
-                        </a>
-                    </i>
-                </li>
-            </ul>
+        <div class="item-grid">
+            <div class="item certificate">
+                <div class="item-image">
+                    <img src="../images/pb-cplus.jpeg" alt="softuni">
+                </div>
+                <div class="item-text">
+                    <div class="item-text-wrapper">
+                        <p class="item-text-dek">SoftUni</p>
+                        <h2 class="item-text-title">Programming Basics with C++</h2>
+                    </div>
+                    <a class="item-link" href="https://softuni.bg/certificates/details/59153/6b5ef0a8"></a>
+                </div>
+            </div>
         </div>
     </section>
 </template>
