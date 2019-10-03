fc1dddd49afce16ed77bf406d272dc4dc713aa70
Ivo <frostwolfe91@gmail.com>
2019-10-03 15:49:47 +0300

    Update Education information

--- resources/js/layouts/certificates.vue
+++ resources/js/layouts/certificates.vue
@@ -13,6 +13,30 @@
                     <a class="item-link" href="https://softuni.bg/certificates/details/59153/6b5ef0a8"></a>
                 </div>
             </div>
+            <div class="item certificate">
+                <div class="item-image">
+                    <img src="../images/php-web.jpeg" alt="softuni">
+                </div>
+                <div class="item-text">
+                    <div class="item-text-wrapper">
+                        <p class="item-text-dek">SoftUni</p>
+                        <h2 class="item-text-title">PHP Web Basics</h2>
+                    </div>
+                    <a class="item-link" href="https://softuni.bg/certificates/details/68424/69573c24"></a>
+                </div>
+            </div>
+            <div class="item certificate">
+                <div class="item-image">
+                    <img src="../images/php-mvc.jpeg" alt="softuni">
+                </div>
+                <div class="item-text">
+                    <div class="item-text-wrapper">
+                        <p class="item-text-dek">SoftUni</p>
+                        <h2 class="item-text-title">PHP MVC Frameworks - Symfony</h2>
+                    </div>
+                    <a class="item-link" href="https://softuni.bg/certificates/details/70590/ac4e695e"></a>
+                </div>
+            </div>
         </div>
     </section>
 </template>
